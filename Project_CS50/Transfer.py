f60d466cdc4f05b1aebe8874fe3b6e29046c82dd
yuanzexi <yuanzexi63@gmail.com>
2018-12-26 10:31:12 +0800

    update transfer

--- Project_CS50/Transfer.py
+++ Project_CS50/Transfer.py
@@ -15,63 +15,92 @@ def parse2AMJ(reference):
         return JournalPublished2AMJ(reference)
 
 
+def parse2AMR(reference):
+    if reference.getCategory() == Defs.BookPublished:
+        return BookPublished2AMR(reference)
+    elif reference.getCategory() == Defs.ChapterPublished:
+        return ChapterPublished2AMR(reference)
+    elif reference.getCategory() == Defs.JournalPublished:
+        return JournalPublished2AMR(reference)
+
+
+def BookPublished2AMR(book):
+    author_str = getAuthorStr(book.getAuthors())
+    return "{0} {1}. <i><b>{2}</b></i>. {3}{4}: {5}.".format(author_str, book.getYear(), book.getTitle(),
+                                                             book.getCity(),
+                                                             ", " + book.getState() if book.getState() else "",
+                                                             book.getPublisher())
+
+
+def ChapterPublished2AMR(chapter):
+    author_str = getAuthorStr(chapter.getAuthors())
+    editor_str = getEditorStr(chapter.getEditors())
+    return "{0} {1}. {2}. In {3}, <i><b>{4}</b></i>: {5}. {6}{7}: {8}.".format(author_str,
+                                                                               chapter.getYear(),
+                                                                               chapter.getChapterTitle(),
+                                                                               editor_str,
+                                                                               chapter.getBookTitle() + (
+                                                                                   "" if chapter.getPageAddition() else chapter.getPageAddition()),
+                                                                               chapter.getStartPage() + "-" + chapter.getEndPage(),
+                                                                               chapter.getCity(),
+                                                                               ", " + chapter.getState() if chapter.getState() else "",
+                                                                               chapter.getPublisher())
+
+
+def JournalPublished2AMR(journal):
+    author_str = getAuthorStr(journal.getAuthors())
+    return "{0} {1}. {2}. <i><b>{3}</b></i>, {4}{5}: {6}.".format(author_str, journal.getYear(), journal.getTitle(),
+                                                                  journal.getJournal(),
+                                                                  journal.getVolume(),
+                                                                  "(" + journal.getIssue() + ")" if journal.getIssue() else "",
+                                                                  journal.getStartPage() + "-" + journal.getEndPage())
+
+
 def BookPublished2AMJ(book):
-    authors = book.getAuthors()
-    author_str = authors[0].toString()
-    for author in authors[1:-1]:
-        author_str += ", " + author.toString()
-    if (len(authors)) > 1:
-        author_str += ", & " + authors[-1].toString()
+    author_str = getAuthorStr(book.getAuthors())
     return "{0} {1}. <i><b>{2}</b></i>. {3}, {4}: {5}.".format(author_str, book.getYear(), book.getTitle(),
                                                                book.getCity(), book.getState(),
                                                                book.getPublisher())
 
 
 def ChapterPublished2AMJ(chapter):
-    authors = chapter.getAuthors()
-    author_str = authors[0].toString()
-    for author in authors[1:-1]:
-        author_str += ", " + author.toString()
-    if (len(authors)) > 1:
-        author_str += ", & " + authors[-1].toString()
-
-    editors = chapter.getEditors()
-    editor_str = editors[0].toReverseString()
-    for editor in editors[1:-1]:
-        editor_str += ", " + editor.toReverseString()
-    if (len(editors)) > 1:
-        editor_str += " & " + editors[-1].toReverseString()
-        return "{0} {1}. {2}. In {3} (Eds.), <i><b>{4}</b></i>: {5}. {6}, {7}: {8}.".format(author_str,
-                                                                                            chapter.getYear(),
-                                                                                            chapter.getChapterTitle(),
-                                                                                            editor_str,
-                                                                                            chapter.getBookTitle() + (
-                                                                                                "" if chapter.getPageAddition() else chapter.getPageAddition()),
-                                                                                            chapter.getStartPage() + "-" + chapter.getEndPage(),
-                                                                                            chapter.getCity(),
-                                                                                            chapter.getState(),
-                                                                                            chapter.getPublisher())
-    else:
-        return "{0} {1}. {2}. In {3} (Ed.), <i><b>{4}</b></i>: {5}. {6}, {7}: {8}.".format(author_str,
-                                                                                           chapter.getYear(),
-                                                                                           chapter.getChapterTitle(),
-                                                                                           editor_str,
-                                                                                           chapter.getBookTitle() + (
-                                                                                               "" if chapter.getPageAddition() else chapter.getPageAddition()),
-                                                                                           chapter.getStartPage() + "-" + chapter.getEndPage(),
-                                                                                           chapter.getCity(),
-                                                                                           chapter.getState(),
-                                                                                           chapter.getPublisher())
+    author_str = getAuthorStr(chapter.getAuthors())
+    editor_str = getEditorStr(chapter.getEditors())
+    return "{0} {1}. {2}. In {3}, <i><b>{4}</b></i>: {5}. {6}, {7}: {8}.".format(author_str,
+                                                                                 chapter.getYear(),
+                                                                                 chapter.getChapterTitle(),
+                                                                                 editor_str,
+                                                                                 chapter.getBookTitle() + (
+                                                                                     "" if chapter.getPageAddition() else chapter.getPageAddition()),
+                                                                                 chapter.getStartPage() + "-" + chapter.getEndPage(),
+                                                                                 chapter.getCity(),
+                                                                                 chapter.getState(),
+                                                                                 chapter.getPublisher())
 
 
 def JournalPublished2AMJ(journal):
-    authors = journal.getAuthors()
-    author_str = authors[0].toString()
-    for author in authors[1:-1]:
-        author_str += ", " + author.toString()
-    if (len(authors)) > 1:
-        author_str += ", & " + authors[-1].toString()
+    author_str = getAuthorStr(journal.getAuthors())
     return "{0} {1}. {2}. <i><b>{3}</b></i>, {4}: {5}.".format(author_str, journal.getYear(), journal.getTitle(),
                                                                journal.getJournal(),
                                                                journal.getVolume(),
                                                                journal.getStartPage() + "-" + journal.getEndPage())
+
+
+def getAuthorStr(authors, cat1=", ", cat2=", & "):
+    author_str = authors[0].toString()
+    for author in authors[1:-1]:
+        author_str += cat1 + author.toString()
+    if (len(authors)) > 1:
+        author_str += cat2 + authors[-1].toString()
+    return author_str
+
+
+def getEditorStr(editors, cat1 = ", ", cat2 = " & ", eds = " (Eds.)", ed=" (Ed.)"):
+    editor_str = editors[0].toReverseString()
+    for editor in editors[1:-1]:
+        editor_str += cat1 + editor.toReverseString()
+    if (len(editors)) > 1:
+        editor_str += cat2 + editors[-1].toReverseString() + eds
+    else:
+        editor_str += ed
+    return editor_str
