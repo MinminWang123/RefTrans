bbad17b4c954fa9425ee88154b1a3b395292380b
yuanzexi <yuanzexi63@gmail.com>
2018-12-29 20:39:01 +0800

    update transfer.py

--- Project_CS50/Transfer.py
+++ Project_CS50/Transfer.py
@@ -42,61 +42,68 @@ def parse_to_hr(reference):
 def book_published_amj(book):
     author_str = get_author_str(book.get_authors())
     title_str = book_title_filter(book.get_title())
-    return "{0} {1}. {2}. {3}: {4}.".format(author_str, book.get_year(), title_str,
-                                               book.get_location(),
-                                               book.get_publisher())
+    return "{0} {1}. {2} {3}: {4}.".format(author_str, book.get_year(), title_str,
+                                           book.get_location(),
+                                           book.get_publisher())
 
 
 def chapter_published_amj(chapter):
     author_str = get_author_str(chapter.get_authors())
     editor_str = get_reverse_editor_str(chapter.get_editors())
-    return "{0} {1}. {2}. In {3}, <i><b>{4}</b></i>{5}: {6}. {7}: {8}.".format(author_str,
-                                                                                  chapter.get_year(),
-                                                                                  chapter.get_chapter_title(),
-                                                                                  editor_str,
-                                                                                  chapter.get_book_title(),
-                                                                                  ", " + chapter.get_page_addition() if chapter.get_page_addition() else "",
-                                                                                  chapter.get_start_page() + "–" + chapter.get_end_page(),
-                                                                                  chapter.get_location(),
-                                                                                  chapter.get_publisher())
+    chapter_title = general_title_filter(chapter.get_chapter_title())
+    book_title = book_title_filter(chapter.get_chapter_title(), end="")
+    return "{0} {1}. {2} In {3}, {4}{5}: {6}. {7}: {8}.".format(author_str,
+                                                                chapter.get_year(),
+                                                                chapter_title,
+                                                                editor_str,
+                                                                book_title,
+                                                                ", " + chapter.get_page_addition() if chapter.get_page_addition() else "",
+                                                                chapter.get_start_page() + "–" + chapter.get_end_page(),
+                                                                chapter.get_location(),
+                                                                chapter.get_publisher())
 
 
 def journal_published_amj(journal):
     author_str = get_author_str(journal.get_authors())
-    return "{0} {1}. {2}. <i><b>{3}</b></i>, {4}{5}: {6}.".format(author_str, journal.get_year(), journal.get_title(),
-                                                                  journal.get_journal(),
-                                                                  journal.get_volume(),
-                                                                  "(" + journal.get_issue() + ")" if journal.get_issue() else "",
-                                                                  journal.get_start_page() + "–" + journal.get_end_page())
+    title = general_title_filter(journal.get_title(), end=".")
+    return "{0} {1}. {2} <i><b>{3}</b></i>, {4}{5}: {6}.".format(author_str, journal.get_year(), title,
+                                                                 journal.get_journal(),
+                                                                 journal.get_volume(),
+                                                                 "(" + journal.get_issue() + ")" if journal.get_issue() else "",
+                                                                 journal.get_start_page() + "–" + journal.get_end_page())
 
 
 def book_inpress_amj(book):
     author_str = get_author_str(book.get_authors())
     title_str = book_title_filter(book.get_title())
-    return "{0} {1}. {2}.".format(author_str, book.get_year(), title_str)
+    return "{0} {1}. {2}".format(author_str, book.get_year(), title_str)
 
 
 def chapter_inpress_amj(chapter):
     author_str = get_author_str(chapter.get_authors())
     editor_str = get_reverse_editor_str(chapter.get_editors())
-    return "{0} {1}. {2}. In {3}, <i><b>{4}</b></i>.".format(author_str,
-                                                             chapter.get_year(),
-                                                             chapter.get_chapter_title(),
-                                                             editor_str,
-                                                             chapter.get_book_title())
+    chapter_title = general_title_filter(chapter.get_chapter_title())
+    book_title = book_title_filter(chapter.get_book_title())
+    return "{0} {1}. {2} In {3}, {4}".format(author_str,
+                                             chapter.get_year(),
+                                             chapter_title,
+                                             editor_str,
+                                             book_title)
 
 
 def journal_inpress_amj(journal):
     author_str = get_author_str(journal.get_authors())
-    return "{0} {1}. {2}. <i><b>{3}</b></i>.".format(author_str, journal.get_year(), journal.get_title(),
-                                                     journal.get_journal())
+    title = general_title_filter(journal.get_title())
+    return "{0} {1}. {2} <i><b>{3}</b></i>.".format(author_str, journal.get_year(), title,
+                                                    journal.get_journal())
 
 
 def online_resources_amj(journal):
     author_str = get_author_str(journal.get_authors())
     journal_str = (("<i><b>" + journal.get_journal() + "</b></i>. ") if journal.get_journal() else "")
-    return "{0} {1}. {2}. {3}Retrieved from {4}".format(author_str, journal.get_year(),
-                                                        journal.get_title(),
+    title = general_title_filter(journal.get_title())
+    return "{0} {1}. {2} {3}Retrieved from {4}".format(author_str, journal.get_year(),
+                                                        title,
                                                         journal_str,
                                                         journal.get_source())
 
@@ -136,81 +143,103 @@ def get_reverse_editor_str(editors, cat1=", ", cat2=" & ", eds=" (Eds.)", ed=" (
     return editor_str
 
 
-def book_title_filter(title, bold=True, italic=True):
+def book_title_filter(title, bold=True, italic=True, end="."):
     title_sub = ""
     new_title = title
     if re.search('\(.*?ed\.\)', title):
         new_title = re.search('(.*?)(\(.*?ed\.\))', title).group(1).strip()
         title_sub = " " + re.search('(.*?)(\(.*?ed\.\))', title).group(2).strip()
+    new_title = general_title_filter(new_title, end=end)
     if bold:
-        new_title = "<b>" + new_title + "</b>"
+        new_title = bold_text(new_title)
     if italic:
-        new_title = "<i>" + new_title + "</i>"
+        new_title = italic_text(new_title)
     return new_title + title_sub
+
+
+def general_title_filter(title, end="."):
+    if title.endswith("?") or title.endswith("!"):
+        return title
+    else:
+        return title + end
+
+
+def bold_text(text):
+    return "<b>" + text + "</b>"
+
+
+def italic_text(text):
+    return "<i>" + text + "</i>"
 
 
 def book_published_hr(book):
     author_str = get_author_str(book.get_authors(), cat1=", ", cat2=" and ", f_cat="", fl_cat=" ")
     title_str = book_title_filter(book.get_title(), bold=False, italic=True)
-    return "{0} ({1}) {2}. {3}: {4}.".format(author_str, book.get_year(), title_str,
-                                                book.get_location(),
-                                                book.get_publisher())
+    return "{0} ({1}) {2} {3}: {4}.".format(author_str, book.get_year(), title_str,
+                                            book.get_location(),
+                                            book.get_publisher())
 
 
 def chapter_published_hr(chapter):
     author_str = get_author_str(chapter.get_authors(), cat1=", ", cat2=" and ", f_cat="", fl_cat=" ")
     editor_str = get_editor_str(chapter.get_editors(), ed=" (ed.)", eds=" (eds)", cat1=", ", cat2=", ", f_cat="",
                                 fl_cat=" ")
-    return "{0} ({1}) {2}. In: {3} <i>{4}</i>. {5}: {6}{7}, {8}.".format(author_str,
-                                                                            chapter.get_year(),
-                                                                            chapter.get_chapter_title(),
-                                                                            editor_str,
-                                                                            chapter.get_book_title(),
-                                                                            chapter.get_location(),
-                                                                            chapter.get_publisher(),
-                                                                            ", " + chapter.get_page_addition() if chapter.get_page_addition() else "",
-                                                                            chapter.get_start_page() + "–" + chapter.get_end_page()
-                                                                            )
+    chapter_title = general_title_filter(chapter.get_chapter_title())
+    book_title = book_title_filter(chapter.get_book_title(), italic=True, bold=False)
+    return "{0} ({1}) {2} In: {3} {4} {5}: {6}{7}, {8}.".format(author_str,
+                                                                chapter.get_year(),
+                                                                chapter_title,
+                                                                editor_str,
+                                                                book_title,
+                                                                chapter.get_location(),
+                                                                chapter.get_publisher(),
+                                                                ", " + chapter.get_page_addition() if chapter.get_page_addition() else "",
+                                                                chapter.get_start_page() + "–" + chapter.get_end_page()
+                                                                )
 
 
 def journal_published_hr(journal):
     author_str = get_author_str(journal.get_authors(), cat1=", ", cat2=" and ", f_cat="", fl_cat=" ")
-    return "{0} ({1}) {2}. <i>{3}</i> {4}{5}: {6}.".format(author_str, journal.get_year(), journal.get_title(),
-                                                           journal.get_journal(),
-                                                           journal.get_volume(),
-                                                           "(" + journal.get_issue() + ")" if journal.get_issue() else "",
-                                                           journal.get_start_page() + "–" + journal.get_end_page())
+    title = general_title_filter(journal.get_title())
+    return "{0} ({1}) {2} <i>{3}</i> {4}{5}: {6}.".format(author_str, journal.get_year(), title,
+                                                          journal.get_journal(),
+                                                          journal.get_volume(),
+                                                          "(" + journal.get_issue() + ")" if journal.get_issue() else "",
+                                                          journal.get_start_page() + "–" + journal.get_end_page())
 
 
 def book_inpress_hr(book):
     author_str = get_author_str(book.get_authors(), cat1=", ", cat2=" and ", f_cat="", fl_cat=" ")
     title_str = book_title_filter(book.get_title(), bold=False, italic=True)
-    return "{0} ({1}) {2}.".format(author_str, book.get_year(), title_str)
+    return "{0} ({1}) {2}".format(author_str, book.get_year(), title_str)
 
 
 def chapter_inpress_hr(chapter):
     author_str = get_author_str(chapter.get_authors(), cat1=", ", cat2=" and ", f_cat="", fl_cat=" ")
     editor_str = get_editor_str(chapter.get_editors(), ed=" (ed.)", eds=" (eds)", cat1=", ", cat2=", ", f_cat="",
                                 fl_cat=" ")
-    return "{0} ({1}) {2}. In: {3} <i>{4}</i>.".format(author_str,
-                                                       chapter.get_year(),
-                                                       chapter.get_chapter_title(),
-                                                       editor_str,
-                                                       chapter.get_book_title()
-                                                       )
+    chapter_title = general_title_filter(chapter.get_chapter_title())
+    book_title = book_title_filter(chapter.get_book_title(), italic=True, bold=False)
+    return "{0} ({1}) {2} In: {3} {4}".format(author_str,
+                                              chapter.get_year(),
+                                              chapter_title,
+                                              editor_str,
+                                              book_title)
 
 
 def journal_inpress_hr(journal):
     author_str = get_author_str(journal.get_authors(), cat1=", ", cat2=" and ", f_cat="", fl_cat=" ")
-    return "{0} ({1}) {2}. <i>{3}</i>.".format(author_str, journal.get_year(), journal.get_title(),
+    title = general_title_filter(journal.get_title())
+    return "{0} ({1}) {2} <i>{3}</i>.".format(author_str, journal.get_year(), title,
                                                journal.get_journal())
 
 
 def online_resources_hr(journal):
     author_str = get_author_str(journal.get_authors())
     journal_str = (("<i>" + journal.get_journal() + "</i>. ") if journal.get_journal() else "")
-    return "{0} ({1}) {2}. {3}Available at: {4}".format(author_str, journal.get_year(),
-                                                        journal.get_title(),
+    title = general_title_filter(journal.get_title())
+    return "{0} ({1}) {2} {3}Available at: {4}".format(author_str, journal.get_year(),
+                                                        title,
                                                         journal_str,
                                                         journal.get_source())
 
@@ -229,9 +258,9 @@ def main():
     b = BookPublished(b_input)
     j = JournalPublished(j_input)
 
-    c = ChapterInPress(c_input)
-    b = BookInPress(b_input)
-    j = JournalInPress(j_input)
+    # c = ChapterInPress(c_input)
+    # b = BookInPress(b_input)
+    # j = JournalInPress(j_input)
 
     journal_type = "AMR"
     print("****")
